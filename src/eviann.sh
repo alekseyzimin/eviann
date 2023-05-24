a4bb5125e43ce3801b198a55716f43e1517f685d
Aleksey Zimin <alekseyz@salz2.idies.jhu.edu>
2023-05-24 17:53:34 -0400

    max 5 prot at locus

--- src/eviann.sh
+++ src/eviann.sh
@@ -325,7 +325,7 @@ if [ ! -e merge.success ];then
         $max_prot_at_locus=2;
         #if NUM_PROT_SPECIES is 2 or less then it look like we are given a protein homology file for a single species
         #then we allow for more extra proteins per locus
-        $max_prot_at_locus=10 if(int('$NUM_PROT_SPECIES')<=2);
+        $max_prot_at_locus=5 if(int('$NUM_PROT_SPECIES')<=2);
         if($h{$F[1]} < $max_prot_at_locus){
           $h{$F[1]}+=1;
           $hn{$F[2]}=1;
