9374c92ccd8fb902b7de74dc052cdcbd6211920c
Aleksey Zimin <alekseyz@salz2.idies.jhu.edu>
2023-09-19 15:47:51 -0400

    added minimap2 to the list of dependencies to check

--- src/eviann.sh
+++ src/eviann.sh
@@ -124,7 +124,7 @@ done
 GENOME=`basename $GENOMEFILE`
 PROTEIN=`basename $PROTEINFILE`
 #checking is dependencies are installed
-for prog in $(echo "ufasta hisat2 stringtie gffread blastp tblastn makeblastdb gffcompare TransDecoder.Predict TransDecoder.LongOrfs");do
+for prog in $(echo "ufasta hisat2 minimap2 stringtie gffread blastp tblastn makeblastdb gffcompare TransDecoder.Predict TransDecoder.LongOrfs");do
   echo -n "Checking for $prog on the PATH... " && \
   which $prog > /dev/null && echo "OK" || error_exit "$prog not found the the PATH, please install the appropriate package";
 done
