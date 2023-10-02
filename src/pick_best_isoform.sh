5bc4053db93d25f96269ec159337082c43ac388f
Aleksey Zimin <alekseyz@salz2.idies.jhu.edu>
2023-10-02 14:09:55 -0400

    added script to pick a single longest isoform for orfanage incorporation

---
+++ src/pick_best_isoform.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PREFIX=$1;
+ufasta one  ../$PREFIX.functional_note.proteins.fasta | \
+awk '{if($1 ~/^>/){pn=substr($1,2);if($2 ~ /Similar/){s=10000}else{s=0}}else{print pn" "s+length($1)}}' |\
+sort -nrk2,2 -S10% |\
+perl -ane '{
+  @f=split(/-/,$F[0]);
+  if(not(defined($h{$f[0]}))){
+    $h{$f[0]}=$F[0];
+    $hh{$F[0]}=1;
+  }
+}END{
+  open(FILE,"../'$PREFIX'.functional_note.pseudo_label.gff");
+  while($line=<FILE>){
+    chomp($line);
+    @f=split(/\t/,$line);
+    @ff=split(/;/,$f[8]);
+    ($junk,$id)=split(/=/,$ff[0]);
+    ($junk,$parent)=split(/=/,$ff[1]);
+    print $line,"\n" if(defined($hh{$id}) || defined($hh{$parent}));
+  }
+}' > $PREFIX.bestisoform.gff
