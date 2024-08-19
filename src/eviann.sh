a40a60beacec8d8945c19291301b06d3621d3879
Aleksey Zimin <aleksey.zimin@gmail.com>
2024-08-19 14:22:43 -0400

    added u CDSs to protein file for TransDecoder refinement

--- src/eviann.sh
+++ src/eviann.sh
@@ -559,7 +559,7 @@ if [ -e transcripts_merge.success ] && [ -e protein2genome.align.success ] && [ 
     mv $GENOME.good_cds.fa.tmp $GENOME.good_cds.fa && \
     mv $GENOME.broken_cds.fa.tmp $GENOME.broken_cds.fa && \
     mv $GENOME.broken_ref.txt.tmp $GENOME.broken_ref.txt && \
-    ufasta extract -f $GENOME.broken_ref.txt $PROTEINFILE > $GENOME.broken_ref.faa.tmp &&\
+    cat $PROTEINFILE <(gffread -y /dev/stdout -g $GENOMEFILE $GENOME.u.cds.gff) | ufasta extract -f $GENOME.broken_ref.txt > $GENOME.broken_ref.faa.tmp &&\
     mv $GENOME.broken_ref.faa.tmp $GENOME.broken_ref.faa && \
     rm -rf $GENOME.broken_cds.fa.transdecoder* && \
     TransDecoder.LongOrfs -S -t $GENOME.broken_cds.fa 1>transdecoder.LongOrfs.out 2>&1 && \
@@ -587,7 +587,7 @@ if [ -e transcripts_merge.success ] && [ -e protein2genome.align.success ] && [ 
     mv $GENOME.good_cds.fa.tmp $GENOME.good_cds.fa && \
     mv $GENOME.broken_cds.fa.tmp $GENOME.broken_cds.fa && \
     mv $GENOME.broken_ref.txt.tmp $GENOME.broken_ref.txt && \
-    ufasta extract -f $GENOME.broken_ref.txt $PROTEINFILE > $GENOME.broken_ref.faa.tmp &&\
+    cat $PROTEINFILE <(gffread -y /dev/stdout -g $GENOMEFILE $GENOME.u.cds.gff) |ufasta extract -f $GENOME.broken_ref.txt > $GENOME.broken_ref.faa.tmp &&\
     mv $GENOME.broken_ref.faa.tmp $GENOME.broken_ref.faa && \
     rm -rf $GENOME.broken_cds.fa.transdecoder* && \
     TransDecoder.LongOrfs -t $GENOME.broken_cds.fa 1>transdecoder.LongOrfs.out 2>&1 && \
