8f366aca1f5e860622a76dda0af3a126b6df2e10
Aleksey Zimin <aleksey.zimin@gmail.com>
2024-11-25 15:49:42 -0500

    improvement in filtering with TPM

--- src/filter_by_local_abundance.pl
+++ src/filter_by_local_abundance.pl
@@ -2,7 +2,6 @@
 #this code filters the transcripts by local abundance; for the same location (gene) and the same CDS, it keeps the best expressed ones
 #input is the GTF file, where the name of the transcript includes the abundance
 #transcript_id "MSTRG_00000160:8"; gene_id "XLOC_000001"; xloc "XLOC_000001"; cmp_ref "NP_051101.1.NC_000932.1.81474"; class_code "k"; tss_id "TSS1";
-my $power=.66;#this should vary between 0 and 1
 my %transcripts_at_xloc_same_cds=();
 while(my $line=<STDIN>){
 next if($line=~/^#/);
@@ -28,7 +27,7 @@ if($gtf_fields[2] eq "transcript"){
 for $l(keys %transcripts_at_xloc_same_cds){
   my @transcripts=sort by_abundance split(/\s/,$transcripts_at_xloc_same_cds{$l});
   my ($tr,$top_count,$top_tpm,$top_class)=split(/:/,$transcripts[0]);
-  my $threshold=weight_function($top_count,$top_tpm)**$power;
+  my $threshold=weight_function($top_count,$top_tpm)**.66;
   #print "DEBUG top $tr count $top_count class $class threshold $threshold\n";
   for(my $i=0;$i<=$#transcripts;$i++){
     my ($tr,$count,$tpm,$class)=split(/:/,$transcripts[$i]);
@@ -45,5 +44,5 @@ return(weight_function($fb[1],$fb[2]) <=> weight_function($fa[1],$fa[2]));
 }
  
 sub weight_function{
-  return($_[1]**.5*$_[0]);
+  return($_[0]*$_[1]**0.1);
 }
