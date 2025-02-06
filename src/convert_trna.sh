2658e1ef81def065f7ccf1a42e97d8c0938c54ef
Aleksey Zimin <aleksey.zimin@gmail.com>
2025-02-06 14:39:46 -0500

    added small script to extract and convert trna genes

---
+++ src/convert_trna.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+perl -F'\t' -ane '{if($F[2] eq "tRNA"){@f=split(/;/,$F[8]);$id=substr($f[0],3);$F[2]="gene";print join("\t",@F);$F[2]="tRNA";print join("\t",@F[0..7]),"\tID=tRNA-$id;Parent=$id\n";$F[2]="exon";print join("\t",@F[0..7]),"\tID=tRNA-exon-$id;Parent=tRNA-$id\n";}}' 
