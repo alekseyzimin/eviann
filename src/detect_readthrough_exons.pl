e058a5fd5626c0023bb643d3500123a3a7d58213
Aleksey Zimin <aleksey.zimin@gmail.com>
2025-02-21 13:36:12 -0500

    more careful elimination of readthrough exons

--- src/detect_readthrough_exons.pl
+++ src/detect_readthrough_exons.pl
@@ -12,25 +12,23 @@ while($line=<FILE>){
   }
 }
 
+my $protid="";
+my $transcript="";
 while($line=<STDIN>){
   chomp($line);
   @F=split(/\t/,$line);
   if($F[2] eq "transcript"){
     $flag=0;
-    if($F[8]=~/^transcript_id "(\S+)"; gene_id (.+); cmp_ref "(\S+)"; class_code "(k|=|j|c|m|n|o)"/){
+    if($F[8]=~/^transcript_id "(\S+)"; gene_id (.+); cmp_ref "(\S+)"; class_code "(k|=|j|c|m|n|o|i|y)"/){
       $protid=$3;
       $tid=$1;
       @tr=split(/\./,$1);
       $transcript=join(".",@tr[0..($#tr-1)])." ".$tr[-1];
-      if($F[6] eq "+"){
-        $flag=1 if($tid =~ /5p$/ && $pend{$protid}<$F[4]);
-        $flag=1 if($tid =~ /3p$/ && $pbeg{$protid}>$F[3]);
-      }else{
-        $flag=1 if($tid =~ /5p$/ && $pbeg{$protid}>$F[3]);
-        $flag=1 if($tid =~ /3p$/ && $pend{$protid}<$F[4]);
-      }
+      $flag=1 if((($tid =~ /5p$/ && $pend{$protid}<$F[4])||($tid =~ /3p$/ && $pbeg{$protid}>$F[3])) && $F[6] eq "+");
+      $flag=1 if((($tid =~ /5p$/ && $pbeg{$protid}>$F[3])||($tid =~ /3p$/ && $pend{$protid}<$F[4])) && $F[6] eq "-");
     }
   }elsif($F[2] eq "exon" && $flag){
-    print "$transcript $F[3] $F[4] $F[6]\n";
+      print "$transcript $F[3] $F[4] $F[6] $pbeg{$protid} $pend{$protid}\n" if((($tid =~ /5p$/ && $pend{$protid}>$F[3])||($tid =~ /3p$/ && $pbeg{$protid}<$F[4])) && $F[6] eq "+");
+      print "$transcript $F[3] $F[4] $F[6] $pbeg{$protid} $pend{$protid}\n" if((($tid =~ /5p$/ && $pbeg{$protid}<$F[4])||($tid =~ /3p$/ && $pend{$protid}>$F[3])) && $F[6] eq "-");
   }
 }
